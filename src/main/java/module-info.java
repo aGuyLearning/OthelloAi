e16172bb75467b2435cb351f03581b2ec54a35ef
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2022-11-04 20:27:11 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module othello.othello {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens othello.othello to javafx.fxml;
+    exports othello.othello;
+}
