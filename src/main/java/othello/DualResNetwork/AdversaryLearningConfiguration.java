f4ae83ee17dfbf9f40a59bf348ffde4e24404131
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2022-12-11 15:38:22 +0100

    more

--- src/main/java/othello/DualResNetwork/AdversaryLearningConfiguration.java
+++ src/main/java/othello/DualResNetwork/AdversaryLearningConfiguration.java
@@ -15,25 +15,25 @@ import java.nio.file.Paths;
  */
 public class AdversaryLearningConfiguration {
   public static double learningRate = 1e-4;
-  public static int batchSize = 16;
+  public static int batchSize = 64;
 
   public static double dirichletAlpha = 1.1;
   public static double dirichletWeight = 0.45;
   public static boolean alwaysUpdateNeuralNetwork = false;
   public static int numberOfGamesToDecideUpdate = 6;
   public static double gamesWinRatioThresholdNewNetworkUpdate = 0.55;
-  public static int numberOfIterationsBeforePotentialUpdate = 10;
+  public static int numberOfIterationsBeforePotentialUpdate = 30;
   public static int iterationStart = 1;
   public static int numberOfIterations = 250;
   public static int checkPointIterationsFrequency = 50;
   public static int fromNumberOfIterationsTemperatureZero = -1;
   public static int fromNumberOfMovesTemperatureZero = 3;
-  public static int maxTrainExamplesHistory = 5000;
+  public static int maxTrainExamplesHistory = 10000;
 
   public static String bestModelFileName = "bestmodel.bin";
   public static String trainExamplesFileName = "trainExamples.obj";
 
-  public static int numberOfMonteCarloSimulations = 3;
+  public static int numberOfMonteCarloSimulations = 5000;
 
   public static String getAbsoluteModelPathFrom(String modelName) {
 
